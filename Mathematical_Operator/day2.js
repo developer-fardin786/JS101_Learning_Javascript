019fa70d70e9533397a661f5a5e03b988b1a4d3b
developer-fardin786 <developerfardin2.0@gmail.com>
2023-08-30 09:54:19 +0530

    added Arithmetic Operators

---
+++ Mathematical_Operator/day2.js
@@ -0,0 +1,12 @@
+var a=19
+var b=9
+let multiple=a*b
+let divide=a/b
+let sub=a-b 
+let exp=a**b
+let modu=a%b
+console.log(multiple)
+console.log(divide)
+console.log(sub)
+console.log(exp)
+console.log(modu)
